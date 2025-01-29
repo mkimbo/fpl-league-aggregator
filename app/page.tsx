5bcc517184c2319f6abf497adb73da481ad2aca9
Jack <nickihllj@gmail.com>
2025-01-29 18:19:36 +0300

    removed default Id

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      leagueId: "1641599",
+      leagueId: "",
     },
   })
 
